41b75f1e8586ab5be175a458e954bd833db818e7
shone1991 <shone1991@mail.ru>
2018-04-23 17:50:47 +0500

    second commit

--- src/main/java/com/ivc/authentication/MyDBAuthenticationService.java
+++ src/main/java/com/ivc/authentication/MyDBAuthenticationService.java
@@ -28,6 +28,7 @@ public class MyDBAuthenticationService implements UserDetailsService {
     public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
         Account account = accountDAO.findAccount(username);
         System.out.println("Account= " + account);
+        System.out.println("Account= " + account);
  
         if (account == null) {
             throw new UsernameNotFoundException("User " //
